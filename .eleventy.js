57a46d32f981129691b52b713d95c5014641fc1f
Michael Champlin <mjchamplin@gmail.com>
2024-06-06 15:53:39 -0500

    Adding tag filters to config

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,21 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  
+
+    // Return all the tags used in a collection
+  eleventyConfig.addFilter("getAllTags", collection => {
+    let tagSet = new Set();
+    for(let item of collection) {
+      (item.data.tags || []).forEach(tag => tagSet.add(tag));
+    }
+    return Array.from(tagSet);
+  });
+
+  eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
+    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+  });
+
   return {
     dir: {
       input: './src',
